9442a55fd21e097045101933e87991c98f551001
jtother <juanramonquiroz15@gmail.com>
2020-04-11 12:46:16 -0500

    added images and some js

---
+++ app.js
@@ -0,0 +1,12 @@
+//created words for game
+let words = ['JAZZ', 'COUNTRY', 'BLUEGRASS'];
+console.log(words);
+const letters = document.querySelector('.button-letters').innerText;{
+letters.addEventListener('click', handleButtonClick) 
+    function handleButtonClick() {
+        console.log(letters);
+    }
+}
+
+
+
